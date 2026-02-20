3f09bfa2a4a7310328e0279889c586a9c0db2d17
Carlos Victor <carlosvictorlimadecarvalho@gmail.com>
2026-02-20 12:42:06 -0300

    first landing page

---
+++ js/js.js
@@ -0,0 +1,10 @@
+function saibaMais(){
+    window.location.href = "mais.html";
+}
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector("#pt1").classList.add("ativo");
+        document.querySelector("#pt2").classList.add("ativo");
+    }, 100);
+});
+let a
