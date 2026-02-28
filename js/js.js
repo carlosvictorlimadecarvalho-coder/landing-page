7d4b1bf215db15a75681d5d060374370456731e7
Carlos Victor <carlosvictorlimadecarvalho@gmail.com>
2026-02-28 20:50:48 -0300

    alteracaobolsonaro17

--- js/js.js
+++ js/js.js
@@ -5,15 +5,16 @@ window.addEventListener("load", () => {
     setTimeout(() => {
         document.querySelector("#pt1").classList.add("ativo");
         document.querySelector("#pt2").classList.add("ativo");
-    }, 100);
+    }, 300);
 });
 const welcome = document.querySelector('.welcome');
 const header = document.querySelector('.header');
 window.addEventListener("scroll", function(){
     if(this.window.scrollY>50){
         welcome.classList.add("small");
-        
+        welcome.style.display = 'flex';
     } else{
         welcome.classList.remove("small");
+        
     }
 });
